c2cfd22fe2e60a8a886b54db91ed7befa39a4db1
sAnjali12 <anjalisen060@gmail.com>
2019-06-06 10:51:30 +0530

    tacking invalid input

--- new_game.py
+++ new_game.py
@@ -1,5 +1,9 @@
 import random
 game_list = ["rock","paper","scissor"]
 computer = random.choice(game_list)
-user_inpur =raw_input("enter your choice rock paper scissor ")
-
+i = 0
+while True:
+	user_input =raw_input("enter your choice rock paper scissor ")
+	if user_input not in game_list:
+		print "invalid input"
+	
